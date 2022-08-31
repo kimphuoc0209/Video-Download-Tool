ddce90083892033d51272a2fedd8ba71e9748094
nhokshiroupro <shiroutran@gmail.com>
2022-08-31 16:23:19 +0700

    Update Scripts

--- main.js
+++ main.js
@@ -3,102 +3,103 @@ const { autoUpdater } = require("electron-updater");
 let win, secondaryWindow;
 
 function createWindow() {
-	let isTransparent = false;
-	if (process.platform == "linux") {
-		isTransparent = true;
-	}
+  let isTransparent = false;
+  if (process.platform == "linux") {
+    isTransparent = true;
+  }
 
-	win = new BrowserWindow({
-		show: false,
-		icon: __dirname + "/public/icon.png",
-		spellcheck: false,
-		transparent: isTransparent,
-		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false,
-		},
-	});
+  win = new BrowserWindow({
+    show: false,
+    icon: __dirname + "/public/icon.png",
+    spellcheck: false,
+    transparent: isTransparent,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
-	win.loadFile("html/index.html");
-	win.maximize();
-	// win.setMenu(null)
-	win.show();
-	// win.webContents.openDevTools();
-	autoUpdater.checkForUpdatesAndNotify();
+  win.loadFile("html/index.html");
+  win.maximize();
+  // win.setMenu(null)
+  win.show();
+  // win.webContents.openDevTools();
+  autoUpdater.checkForUpdatesAndNotify();
 }
 
 app.whenReady().then(() => {
-	createWindow();
-	app.on("activate", () => {
-		if (BrowserWindow.getAllWindows().length === 0) {
-			createWindow();
-		}
-	});
-	if (process.platform === "win32") {
-		app.setAppUserModelId(app.name);
-	}
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+  if (process.platform === "win32") {
+    app.setAppUserModelId(app.name);
+  }
 });
 
 ipcMain.on("load-page", (event, file) => {
-	secondaryWindow = new BrowserWindow({
-		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false,
-		},
-		parent: win,
-		modal: true,
-		show: false,
-	});
-	// win.loadFile(file);
-	secondaryWindow.loadFile(file);
-	secondaryWindow.setMenu(null)
-	secondaryWindow.maximize();
-	secondaryWindow.show();
+  secondaryWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    parent: win,
+    modal: true,
+    show: false,
+  });
+  // win.loadFile(file);
+  secondaryWindow.loadFile(file);
+  secondaryWindow.setMenu(null);
+  secondaryWindow.maximize();
+  secondaryWindow.show();
 });
 
 ipcMain.on("close-secondary", () => {
-	secondaryWindow.close();
-	secondaryWindow = null;
+  secondaryWindow.close();
+  secondaryWindow = null;
 });
 
 ipcMain.on("select-location", () => {
-	const location = dialog.showOpenDialogSync(secondaryWindow, {
-		properties: ["openFile", "openDirectory"],
-	});
+  const location = dialog.showOpenDialogSync(secondaryWindow, {
+    properties: ["openFile", "openDirectory"],
+  });
 
-	if (location) {
-		secondaryWindow.webContents.send("downloadPath", location);
-	}
+  if (location) {
+    secondaryWindow.webContents.send("downloadPath", location);
+  }
 });
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 // Auto updater events
 
 autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
-	const dialogOpts = {
-		type: "info",
-		buttons: ["Ok"],
-		title: "Application Update",
-		message: process.platform === "win32" ? releaseNotes : releaseName,
-		detail: "A new version is being downloaded.",
-	};
-	dialog.showMessageBox(dialogOpts, (response) => {});
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Ok"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail: "A new version is being downloaded.",
+  };
+  dialog.showMessageBox(dialogOpts, (response) => {});
 });
 
 autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
-	const dialogOpts = {
-		type: "info",
-		buttons: ["Restart", "Later"],
-		title: "Application Update",
-		message: process.platform === "win32" ? releaseNotes : releaseName,
-		detail: "A new version has been downloaded. Restart the application to apply the updates.",
-	};
-	dialog.showMessageBox(dialogOpts).then((returnValue) => {
-		if (returnValue.response === 0) autoUpdater.quitAndInstall();
-	});
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Restart", "Later"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail:
+      "A new version has been downloaded. Restart the application to apply the updates.",
+  };
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  });
 });
--- src/index.js
+++ src/index.js
@@ -4,51 +4,51 @@ const incorrectMsg = getId("incorrectMsg");
 const loadingMsg = getId("loadingWrapper");
 
 function getId(id) {
-	return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 let menuIsOpen = false;
 
 getId("menuIcon").addEventListener("click", (event) => {
-	if (menuIsOpen) {
-		getId("menuIcon").style.transform = "rotate(0deg)";
-		menuIsOpen = false;
-		let count = 0;
-		let opacity = 1;
-		const fade = setInterval(() => {
-			if (count >= 10) {
-				clearInterval(fade);
-			} else {
-				opacity -= 0.1;
-				getId("menu").style.opacity = opacity;
-				count++;
-			}
-		}, 50);
-	} else {
-		getId("menuIcon").style.transform = "rotate(90deg)";
-		menuIsOpen = true;
+  if (menuIsOpen) {
+    getId("menuIcon").style.transform = "rotate(0deg)";
+    menuIsOpen = false;
+    let count = 0;
+    let opacity = 1;
+    const fade = setInterval(() => {
+      if (count >= 10) {
+        clearInterval(fade);
+      } else {
+        opacity -= 0.1;
+        getId("menu").style.opacity = opacity;
+        count++;
+      }
+    }, 50);
+  } else {
+    getId("menuIcon").style.transform = "rotate(90deg)";
+    menuIsOpen = true;
 
-		setTimeout(() => {
-			getId("menu").style.display = "flex";
-			getId("menu").style.opacity = 1;
-		}, 150);
-	}
+    setTimeout(() => {
+      getId("menu").style.display = "flex";
+      getId("menu").style.opacity = 1;
+    }, 150);
+  }
 });
 
 // Video and audio toggle
 
 videoToggle.addEventListener("click", (event) => {
-	videoToggle.style.backgroundColor = "var(--box-toggleOn)";
-	audioToggle.style.backgroundColor = "var(--box-toggle)";
-	getId("audioList").style.display = "none";
-	getId("videoList").style.display = "block";
+  videoToggle.style.backgroundColor = "var(--box-toggleOn)";
+  audioToggle.style.backgroundColor = "var(--box-toggle)";
+  getId("audioList").style.display = "none";
+  getId("videoList").style.display = "block";
 });
 
 audioToggle.addEventListener("click", (event) => {
-	audioToggle.style.backgroundColor = "var(--box-toggleOn)";
-	videoToggle.style.backgroundColor = "var(--box-toggle)";
-	getId("videoList").style.display = "none";
-	getId("audioList").style.display = "block";
+  audioToggle.style.backgroundColor = "var(--box-toggleOn)";
+  videoToggle.style.backgroundColor = "var(--box-toggle)";
+  getId("videoList").style.display = "none";
+  getId("audioList").style.display = "block";
 });
 
 /////////////
@@ -61,71 +61,71 @@ const root = document.querySelector(":root");
 let enabledTransparent = localStorage.getItem("enabledTransparent");
 let bgColor = "";
 if (enabledTransparent == "true") {
-	bgColor = "rgba(40,40,40, .9)";
+  bgColor = "rgba(40,40,40, .9)";
 } else {
-	bgColor = "rgb(40,40,40)";
+  bgColor = "rgb(40,40,40)";
 }
 
 function toggle() {
-	if (darkTheme == false) {
-		circle.style.left = "25px";
+  if (darkTheme == false) {
+    circle.style.left = "25px";
 
-		root.style.setProperty("--background", bgColor);
-		root.style.setProperty("--text", "white");
-		root.style.setProperty("--box-main", "rgb(80,80,80)");
-		root.style.setProperty("--box-toggle", "rgb(70,70,70)");
-		root.style.setProperty("--theme-toggle", "rgb(80, 193, 238)");
-		root.style.setProperty("--item-bg", "rgb(75, 75, 75)");
-		root.style.setProperty("--box-shadow", "none");
+    root.style.setProperty("--background", bgColor);
+    root.style.setProperty("--text", "white");
+    root.style.setProperty("--box-main", "rgb(80,80,80)");
+    root.style.setProperty("--box-toggle", "rgb(70,70,70)");
+    root.style.setProperty("--theme-toggle", "rgb(80, 193, 238)");
+    root.style.setProperty("--item-bg", "rgb(75, 75, 75)");
+    root.style.setProperty("--box-shadow", "none");
 
-		darkTheme = true;
-		localStorage.setItem("theme", "dark");
-	} else {
-		circle.style.left = "0px";
+    darkTheme = true;
+    localStorage.setItem("theme", "dark");
+  } else {
+    circle.style.left = "0px";
 
-		root.style.setProperty("--background", "whitesmoke");
-		root.style.setProperty("--text", "rgb(45, 45, 45)");
-		root.style.setProperty("--box-main", "rgb(174 249 224)");
-		root.style.setProperty("--box-toggle", "rgb(108, 231, 190)");
-		root.style.setProperty("--theme-toggle", "rgb(147, 174, 185)");
-		root.style.setProperty("--item-bg", "#ececec");
-		root.style.setProperty("--box-shadow", "3px 3px 10px gray");
-		root.style.setProperty(
-			"--box-shadow",
-			"2px 2px 5px rgb(92, 92, 92), -2px -2px 5px rgb(219, 219, 219)"
-		);
+    root.style.setProperty("--background", "whitesmoke");
+    root.style.setProperty("--text", "rgb(45, 45, 45)");
+    root.style.setProperty("--box-main", "rgb(174 249 224)");
+    root.style.setProperty("--box-toggle", "rgb(108, 231, 190)");
+    root.style.setProperty("--theme-toggle", "rgb(147, 174, 185)");
+    root.style.setProperty("--item-bg", "#ececec");
+    root.style.setProperty("--box-shadow", "3px 3px 10px gray");
+    root.style.setProperty(
+      "--box-shadow",
+      "2px 2px 5px rgb(92, 92, 92), -2px -2px 5px rgb(219, 219, 219)"
+    );
 
-		darkTheme = false;
-		localStorage.setItem("theme", "light");
-	}
+    darkTheme = false;
+    localStorage.setItem("theme", "light");
+  }
 }
 
 const storageTheme = localStorage.getItem("theme");
 
 if (storageTheme == "dark") {
-	darkTheme = false;
-	toggle();
+  darkTheme = false;
+  toggle();
 } else if (storageTheme == "light") {
-	darkTheme = true;
-	toggle();
+  darkTheme = true;
+  toggle();
 }
 ////
 
 function clickAnimation(id) {
-	getId(id).style.animationName = "clickAnimation";
+  getId(id).style.animationName = "clickAnimation";
 
-	setTimeout(() => {
-		getId(id).style.animationName = "";
-	}, 500);
+  setTimeout(() => {
+    getId(id).style.animationName = "";
+  }, 500);
 }
 let advancedHidden = true;
 
 function advancedToggle() {
-	if (advancedHidden) {
-		getId("advanced").style.display = "block";
-		advancedHidden = false;
-	} else {
-		getId("advanced").style.display = "none";
-		advancedHidden = true;
-	}
+  if (advancedHidden) {
+    getId("advanced").style.display = "block";
+    advancedHidden = false;
+  } else {
+    getId("advanced").style.display = "none";
+    advancedHidden = true;
+  }
 }
